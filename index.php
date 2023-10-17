d9ade972da443662ecee1c9db2f8cf747264e6be
sky-hackeR <jolayemidavid6@gmail.com>
2023-10-17 19:38:33 +0100

    Variable declaration and calling of variables

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$name =  "Ayedun James";
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SWE 305</title>
+</head>
+<body>
+    <h1 style ="color: yellow; background-color: black; padding: 20px">Welcome! <?= $name?> </h1>
+</body>
+</html>
